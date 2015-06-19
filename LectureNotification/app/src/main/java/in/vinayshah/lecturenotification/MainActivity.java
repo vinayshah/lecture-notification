b2702108bb80dfcfd5faf174334a6bd7481b03e6
Vinay Sudhir Shah <vinay.sudhir@gmail.com>
2015-06-20 00:50:18 +0530

    LoginActivity
    Login Activity Code

--- LectureNotification/app/src/main/java/in/vinayshah/lecturenotification/MainActivity.java
+++ LectureNotification/app/src/main/java/in/vinayshah/lecturenotification/MainActivity.java
@@ -1,17 +1,38 @@
 package in.vinayshah.lecturenotification;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+    EditText un,pw;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        un = (EditText) findViewById(R.id.txtUsername );
+        pw = (EditText) findViewById(R.id.txtPassword );
+
+        b1 = (Button) findViewById(R.id.btnLogin);
+        b2 = (Button) findViewById(R.id.btnReset);
+
+        b1.setOnClickListener(new onClickListener(){
+            @Override
+            public void onClick(View arg0){
+//                if( un.equals('vinay') && pw.equals('vinay') ){
+                    console.log('Login');
+//                }
+            }
+        });
     }
 
     @Override
